409dd78e61d30b369776412fe11b797ffdcb479f
João Vicente Campanher <jvcampanher@gmail.com>
2024-08-14 14:33:37 -0300

    getItems

--- src/app/menus/[menuId]/ItemList.tsx
+++ src/app/menus/[menuId]/ItemList.tsx
@@ -1,3 +1,37 @@
+import APICaller from "@/utils/APICaller";
+import { useEffect, useState } from "react";
+
 export default function ItemList({ categoryId }: { categoryId: string }) {
-  return <div className="self-center">list here</div>;
+  const [items, setItems] = useState([]);
+
+  async function getItems() {
+    try {
+      const response = await APICaller(
+        `/api/items?categoryId=${categoryId}`,
+        "GET"
+      );
+      setItems(response.items);
+    } catch (error) {
+      console.error("Erro ao buscar os itens:", error);
+    }
+  }
+
+  useEffect(() => {
+    getItems();
+  }, [categoryId]);
+
+  return (
+    <div className="self-center">
+      {items.map((item: any) => {
+        return (
+          <div key={item.id} className="flex flex-col gap-2">
+            <span className="font-semibold">{item.name}</span>
+            <span>{item.description}</span>
+            <span>{item.shortDescription}</span>
+            <span>{item.price}</span>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
